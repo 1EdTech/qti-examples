6c7115ece5b1073732ca33f76dd8f7c901bc4cc3
padraigo <padraigo@padraig>
2018-03-02 17:23:35 +0000

    Add a simple example QTI content package of packaging a single PCI item

---
+++ packaging/pci-simple/src/modules/module_resolution.js
@@ -0,0 +1,16 @@
+{   "waitSeconds": 60,
+        "paths": {
+            "jquery": "https://code.jquery.com/jquery-2.2.2.min",
+            "helpers": "shared/helpers",
+            "baseBoundTemplate": "shared/baseBoundTemplate",
+            "pnp": "shared/pnp",
+            "eve": "modules/lib/eve",
+            "raphael": "modules/lib/raphael",
+            "handlebars": "https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.10/handlebars.amd.min",
+            "boxMultiplication": "modules/box_multiplication",
+            "boxMultiplication_css": "modules/box_multiplication_css",
+            "shadingD": "modules/shading_deps",
+            "tap": "modules/tap"
+        }
+}
+
